9036d58f020a2ff600ee0c0e4e6624d62c1f9113
Nicolas da Silva Mesquita Oliveira <dragonnightsky5@gmail.com>
2026-04-17 00:02:56 -0300

    avaliacoes: A avaliação 'avaliacao-04' foi concluída com sucesso, mas entrega com atraso.

--- avaliacoes/avaliacao-04/client.dart
+++ avaliacoes/avaliacao-04/client.dart
@@ -1,11 +1,14 @@
-import 'dart:io';
-import 'dart:convert';
+import 'dart:io'; // importando a biblioteca io que fornece as classes utilizadas para as conexões e interações com o sistema operacional (IO) [Socket é um exemplo]
+import 'dart:convert'; // importando a biblioteca convert para a conversão de dados codificados em UTF8, nesse código, para um String
 
 void main() async {
+  // Criando o socket que se conecta com o servidor a partir da porta
   final socketCreated = await Socket.connect('127.0.0.1', 3030);
   
+  // Escreve uma mensagem no terminal do servidor, mostrando que o cliente realizou a conexão
   socketCreated.write('Client message: Hello!\n');
   
+  // Ouve o que é enviado ao cliente, decodifica os dados enviados pelo servidor, imprime no terminal e fecha quando não houve mais dados no socket
   socketCreated.listen(
     (data) => print('Server: ${utf8.decode(data)}'),
     onDone: () => socketCreated.destroy()
--- avaliacoes/avaliacao-04/server.dart
+++ avaliacoes/avaliacao-04/server.dart
@@ -1,66 +1,92 @@
-import 'dart:math';
-import 'dart:io';
-import 'dart:async';
+import 'dart:math'; // improtando a biblioteca math que possui a classe Random
+import 'dart:io'; // importando a biblioteca io que fornece as classes utilizadas para as conexões e interações com o sistema operacional (IO) [Socket é um exemplo]
+import 'dart:async'; // Importando a biblioteca async que fornece as principais classes de manutencão da assincronicidade (Future<T> e Timer são exemplos)
+
+// Criando a função que retorna as temperaturas como valores double de duas casas decimais, que podem variar de -10°C a 35°C
 
 double temperatureGen() {
 
   var random = Random();
   double randomTemperature = -10 + (random.nextDouble() * 45);
   randomTemperature = (randomTemperature * 10).round() / 10;
-  print('- Temperatura: $randomTemperature');
+  print('- Temperature: $randomTemperature°C');
 
   return randomTemperature;
 }
 
+// Criando a função principal responsável por receber os dados da função acima e enviá-los para os clientes
+
 Future<void> periodicBroadcast({
-  required Duration intervalo,
-  required String Function() gerarDados,
-  required Set<Socket> clients,
-  }) async {
-  await for (final _ in Stream.periodic(intervalo)) {
-    final dados = await gerarDados();
+  required Duration intervalo, // A função exige um intervalo (definido para 10 segundos posteriormente)
+  required String Function() gerarDados, // A função exige o retorno de uma função (os valores em double)
+  required Set<Socket> clients, // A função exige os clientes que receberão as temperaturas
+  }) async { // Função assíncrona que executa o envio das temperaturas aos clientes periodicamente
+  Timer.periodic(intervalo, (timer) {
+    final dados = gerarDados();
+    if (clients.length == 0) {
+      print('No one is connected and receiving this data. Looking for clients...');
+    } else {
+      print('Sending $dados to ${clients.length} clients.');
+    };
     
-    clients.removeWhere((c) => c.done == true);
-    
-    for (final client in clients) {
-      client.write(dados);
+    // Imprime as informações diretamente no terminal dos clientes que se conectaram com sucesso
+    for (final client in clients) { 
+      try {
+        client.write(dados);
+      } catch (e) {
+        clients.remove(client);
+        print('Error! The error was identified and registered as "$e.". Client connection has been deactivated.');
+      }
     }
-  }
+  });
 }
 
 void main() async {
+  // Iniciando o servidor e definindo o socket para o(s) cliente(s)
   final serverCreated = await ServerSocket.bind(InternetAddress.anyIPv4, 3030);
   final clients = <Socket>{};
 
+  // Verificando quando o usuário clica Ctrl + C no terminal do servidor e fecha-o
   ProcessSignal.sigint.watch().listen((_) async {
     await serverCreated.close();
     exit(0);
   });
 
+  // Imprime mensagens sobre a conexão do servidor e uma instrução que informa o usuário a apertar Ctrl + C para finalizar o envio de dados pelo servidor
   print('Server adress: ${serverCreated.address.address}, PORT: ${serverCreated.port}');
   print('The server is running. If you wish to stop, press Ctrl+C.');
   print('\n');
 
-  await for (final client in serverCreated) {
-    print('Server started!');
-    clients.add(client);
-    client.listen((_) { print('Client active');},
+  // Envia as informções para a classe 'Future<void> periodicBroadcast'
+  periodicBroadcast(
+    intervalo: Duration(seconds: 10),
+    gerarDados: () => temperatureGen().toString(), // Convertendo o valor para String
+    clients: clients
+  );
 
+  // Função que identifica a conexão realizada pelo(s) cliente(s)
+  await for (final client in serverCreated) {
+    clients.add(client);
+    print('New client connected: ${client.remoteAddress.address}.'); // Mostra o endereço IP do cliente que recebe as temperaturas
+
+    // Recebe uma pequena mensagem de teste do cliente para confirmar a conexão com o servidor
+    client.listen(
+      (data) {
+        print('Client message: ${String.fromCharCodes(data).trim()}');
+      },
+
+      // Identifica quando é acionado o comando Ctrl + C no terminal do cliente e o remove
       onDone: () {
-        print('Client leaving...');
+        print('Client has been disconected.');
         clients.remove(client);
         client.close();
       },
+
+      // Identifica algum erro durante a ligação com o cliente e fecha o cliente
       onError: (_) {
         clients.remove(client);
         client.close();
       }
     );
   }
-
-  await periodicBroadcast(
-    intervalo: Duration(seconds: 10),
-    gerarDados: () => temperatureGen().toStringAsFixed(2),
-    clients: clients
-  );
 }
